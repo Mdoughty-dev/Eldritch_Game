6510f8bd3c51d645422dc5f73e139476a68c7f2d
vertigo1919 <149095893+vertigo1919@users.noreply.github.com>
2026-03-09 18:27:56 +0000

    feat: add testing component for socket accessible via route /socket

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
 import PhaserCanvas from "./components/PhaserCanvas";
+import SocketTesting from "./components/SocketTesting";
+import { Routes, Route } from "react-router-dom";
 
 export default function App() {
-  return <PhaserCanvas />;
+  return (
+    <Routes>
+      {/* Default Game View at http://localhost:5173/ */}
+      <Route path="/" element={<PhaserCanvas />} />
+
+      {/*  Test View for socket debuggint at http://localhost:5173/socket */}
+      <Route path="/socket" element={<SocketTesting />} />
+    </Routes>
+  );
 }
---
+++ src/components/SocketTesting.jsx
@@ -0,0 +1,12 @@
+// import React, { useState } from 'react';
+// import { io } from 'socket.io-client';
+
+const SocketTesting = () => {
+  return (
+    <div>
+      <h1>Socket Testing Dashboard</h1>
+    </div>
+  );
+};
+
+export default SocketTesting;
