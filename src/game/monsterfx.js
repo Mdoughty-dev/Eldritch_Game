f8a3d42ce00b1f80ad06192c466be894ddf0133a
Tvin9 <christopherharrison0606@gmail.com>
2026-03-13 16:07:49 +0000

    Added more fx to encounter scene

--- src/game/monsterfx.js
+++ src/game/monsterfx.js
@@ -1,29 +1,51 @@
-export function playMonsterHitFx(scene, monsterSprite, onComplete) {
-  if (!monsterSprite) return;
+export function playMonsterHitFx(
+	scene,
+	monsterSprite,
+	monsterDamage,
+	onComplete,
+) {
+	if (!monsterSprite) return;
 
-  const startX = monsterSprite.x;
-  const startY = monsterSprite.y;
-  const originalScaleX = monsterSprite.scaleX;
-  const originalScaleY = monsterSprite.scaleY;
+	const startX = monsterSprite.x;
+	const startY = monsterSprite.y;
+	const originalScaleX = monsterSprite.scaleX;
+	const originalScaleY = monsterSprite.scaleY;
 
-  scene.tweens.killTweensOf(monsterSprite);
+	scene.tweens.killTweensOf(monsterSprite);
 
-  monsterSprite.clearTint();
-  monsterSprite.setTint(0xff4444);
-  monsterSprite.setScale(originalScaleX * 1.08, originalScaleY * 1.08);
+	monsterSprite.clearTint();
+	monsterSprite.setTint(0xff4444);
+	monsterSprite.setScale(originalScaleX * 1.08, originalScaleY * 1.08);
 
-  scene.tweens.add({
-    targets: monsterSprite,
-    x: startX + 18,
-    duration: 45,
-    yoyo: true,
-    repeat: 4,
-    onComplete: () => {
-      monsterSprite.setPosition(startX, startY);
-      monsterSprite.clearTint();
-      monsterSprite.setScale(originalScaleX, originalScaleY);
+	scene.tweens.add({
+		targets: monsterSprite,
+		x: startX + 18,
+		duration: 45,
+		yoyo: true,
+		repeat: 4,
+		onComplete: () => {
+			monsterSprite.setPosition(startX, startY);
+			monsterSprite.clearTint();
+			monsterSprite.setScale(originalScaleX, originalScaleY);
 
-      if (onComplete) onComplete();
-    },
-  });
+			if (onComplete) onComplete();
+		},
+	});
+
+	const damagePopup = scene.add.text(920, 200, `${monsterDamage}`, {
+		fontSize: '32px',
+	});
+	const xDirection = Math.random() * (250, -150) + 970;
+
+	scene.tweens.add({
+		targets: damagePopup,
+		x: xDirection,
+		y: 150,
+		duration: 500,
+		ease: 'Linear',
+
+		onComplete: () => {
+			damagePopup.destroy();
+		},
+	});
 }
